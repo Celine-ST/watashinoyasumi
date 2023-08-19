633df88bbda4c17d6f0c8632505b02bc39b9affa
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-08-19 10:37:25 +0800

    Initial Commit

---
+++ watashinoyasumi/watashinoyasumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  watashinoyasumiApp.swift
+//  watashinoyasumi
+//
+//  Created by Celine Quek on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct watashinoyasumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
